2a610c938a14edc5909da03c5649114fc3905d8b
zhangrh <runhaozhang@sohu-inc.com>
2020-07-01 21:15:22 +0800

    init

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # 文档
 
  zip下载地址: `https://zhangrunhao.oss-cn-beijing.aliyuncs.com/offline_zip/test.zip`
+
+## 概述
+
+* 这是放到安卓里面的里面
+* 也是我们需要打包的页面
--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 window.onload = function () {
+  alert(2)
   document.getElementById('btn').addEventListener('click', function () {
-    var str = dsBridge.call("testSyn","testSyn");
+    var str = dsBridge.call("testSyn", "testSyn");
     alert(str)
     dsBridge.call('testAsyn', "testAsyn", function (v) {
       alert(v)
     })
-  })
+  }) 
 }
