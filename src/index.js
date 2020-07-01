f795f3a37b743d0acb46438b525cd12c3c825d36
zhangrh <runhaozhang@sohu-inc.com>
2020-07-02 00:06:14 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
-// window.onload = function () {
-//   alert(2)
-//   document.getElementById('btn').addEventListener('click', function () {
-//     var str = dsBridge.call("testSyn", "testSyn");
-//     alert(str)
-//     dsBridge.call('testAsyn', "testAsyn", function (v) {
-//       alert(v)
-//     })
-//   })
-// }
+// import './index.css';
 
 function component() {
+  console.log(1)
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = ['Hello', 'webpack']
 
   return element;
 }
